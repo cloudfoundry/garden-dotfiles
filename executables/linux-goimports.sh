5674ffd0768ed1f22ebcb01e8482f46b0d3714ca
Danail Branekov <danailster@gmail.com>
2019-08-19 16:30:37 +0100

    ensure goimports exists
    and small install script typo fixes

--- executables/linux-goimports.sh
+++ executables/linux-goimports.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+if [ ! -f "$DIR/goimports" ]; then
+  go build -o $DIR/goimports golang.org/x/tools/cmd/goimports
+fi
 GOOS=linux CGO_ENABLED=0 "$DIR/goimports" "$@"
-
