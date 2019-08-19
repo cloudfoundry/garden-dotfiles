4e8236c76615ffdee218e232693eb1bef23edd44
Danail Branekov <danailster@gmail.com>
2019-08-19 16:40:04 +0100

    No need to install goimports ourselves
    GoUpdateBinaries does it for us

--- executables/linux-goimports.sh
+++ executables/linux-goimports.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-if [ ! -f "$DIR/goimports" ]; then
-  go build -o $DIR/goimports golang.org/x/tools/cmd/goimports
-fi
 GOOS=linux CGO_ENABLED=0 "$DIR/goimports" "$@"
