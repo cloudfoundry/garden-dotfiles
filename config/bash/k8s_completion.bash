2a7276ef37a0c9a1bb564ae8276a9517e5e3787b
Danail Branekov <danailster@gmail.com>
2019-10-31 14:03:53 +0200

    Install kubectl and configure its bash completion
    [#169475540]
    
    Co-authored-by: Kieron Browne <kbrowne@pivotal.io>

---
+++ config/bash/k8s_completion.bash
@@ -0,0 +1,4 @@
+export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
+[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+
+source /usr/local/etc/bash_completion.d/kubectl
